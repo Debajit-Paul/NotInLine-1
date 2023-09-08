42a3e511900bb67809b025b0ce5cbe51cd3ee5ea
Debajit Paul <debajitfan.of@gmail.com>
2023-09-08 03:15:16 -0700

    animations

--- dist/script.js
+++ dist/script.js
@@ -1,43 +1,44 @@
 function menuToggle() {
-  console.log("click");
   let displayIcon = document.getElementById("mobile-menu-btn");
   let menu = document.getElementById("mobile-menu");
+  menu.classList.toggle("left-[0px]");
+  menu.classList.toggle("left-[770px]");
+
   if (displayIcon.src.match("./img/hamburger.svg")) {
     displayIcon.src = "./img/close.svg";
-    menu.classList.add("flex");
-    menu.classList.add("flex-col");
-    menu.classList.remove("hidden");
+    displayIcon.classList.toggle("rotate-90");
   } else {
     displayIcon.src = "./img/hamburger.svg";
-    menu.classList.add("hidden");
-    menu.classList.remove("flex");
-    menu.classList.remove("flex-col");
+    displayIcon.classList.toggle("rotate-90");
   }
 }
 
 function dropdown() {
   let subMenu = document.getElementById("sub-menu");
   let subMenuIcon = document.getElementById("drop-down-arrow");
-  if (subMenu.style.display === "none" || subMenu.style.display === "") {
-    subMenu.style.display = "block";
-    subMenuIcon.classList.add("rotate-180");
-  } else {
-    subMenu.style.display = "none";
-    subMenuIcon.classList.remove("rotate-180");
-  }
+  subMenu.classList.toggle("max-h-0");
+  subMenu.classList.toggle("max-h-22");
+  subMenuIcon.classList.toggle("rotate-0");
+  subMenuIcon.classList.toggle("rotate-180");
+  // if (subMenu.style.display === "none" || subMenu.style.display === "") {
+  //   subMenu.style.display = "block";
+  //   subMenuIcon.classList.add("rotate-180");
+  // } else {
+  //   subMenu.style.display = "none";
+  //   subMenuIcon.classList.remove("rotate-180");
+  // }
 }
 
 function expand(id) {
-  let expandDiv = document.getElementById(`${id}`);
-  if (expandDiv.style.display === "none" || expandDiv.style.display === "") {
-    expandDiv.style.display = "block";
+  let cardIcon = document.getElementById(`${id}`);
+  if (cardIcon.src.match("./img/expand-arrow.svg")) {
+    cardIcon.src = "./img/close.svg";
   } else {
-    expandDiv.style.display = "none";
+    cardIcon.src = "./img/expand-arrow.svg";
   }
 }
 
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
   slidesPerView: 3,
   spaceBetween: 40,
   direction: "horizontal",
